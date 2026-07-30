26b596746608f3820e83841f85677d525e9f8ae8
Steve Shaw <38044085+sm-shaw@users.noreply.github.com>
2026-07-30 14:21:27 +0100

    Update test-maria.sh

--- Docker/tests/test-maria.sh
+++ Docker/tests/test-maria.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
-source "$(dirname "$0")/common.sh"; validate_common
-run bash -euc 'dpkg-query -W -f="${Version}\n" libmariadb3; ldconfig -p | grep libmariadb.so.3'
+
+source "$(dirname "$0")/common.sh"
+validate_common
+
+run bash -euc '
+  dpkg-query -W -f="$1" libmariadb3
+  ldconfig -p | grep -F libmariadb.so.3
+' -- '${Version}\n'
+
 librarycheck '(Success|loaded library).*MariaDB'
