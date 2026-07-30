00fac9a48a6ea032e590e33d3a8b15f39bd0964c
Steve Shaw <38044085+sm-shaw@users.noreply.github.com>
2026-07-30 17:55:32 +0100

    Update test-combined.sh

--- Docker/tests/test-combined.sh
+++ Docker/tests/test-combined.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
-source "$(dirname "$0")/common.sh"; validate_common
-for db in MySQL MariaDB PostgreSQL Oracle 'SQL Server'; do librarycheck "(Success|loaded library).*$db"; done
+
+source "$(dirname "$0")/common.sh"
+validate_common
+
+for db in MySQL MariaDB PostgreSQL Oracle MSSQLServer; do
+  librarycheck "(Success|loaded library).*$db"
+done
